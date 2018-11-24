e0416cbe4dd060185a84e6e05c1def77161e4637
Gabriela <gabriela.oliveira@ccc.ufcg.edu.br>
2018-11-24 13:57:44 -0300

    :tada: router :tada:

--- kitso-books/src/App.js
+++ kitso-books/src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import Book from './components/Book';
 import Author from './components/Author';
-import Navbar from './components/Navbar';
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navbar/>
       <Book/>
       </div>
     );
--- kitso-books/src/index.js
+++ kitso-books/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Book from './components/Book';
+import Author from './components/Author';
+import Navbar from './components/Navbar';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <div>
+    <Navbar/>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={Book} />
+        <Route path="/author" component={Author} />
+        </Switch>
+    </ BrowserRouter>
+  </div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
